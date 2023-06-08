c417bce16c7586fc819f0cd8caa1fd8a15a829ec
douira <douira100@gmail.com>
2023-06-08 04:16:11 +0200

    improve text layout

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,16 @@
     <a href="https://github.com/MCRcortex/nekodetector">nekodetector</a> by
     cortex et al.
   </div>
-  <div style="margin-bottom: 20px">
+  <div style="margin-bottom: 20px; max-width: 800px">
     This tool works by looking for specific sequences of bytecodes within the
-    class files. Be aware of false positives (it's possible but unlikely that a
-    class file contains the same bytecode sequence as this tool is looking for)
-    and false negatives (it's possible that a class file is infected but doesn't
-    contain the same bytecode sequence as this tool is looking for).
+    class files. The files are scannend <i>locally</i> in your browser
+    with JavaScript and aren't sent to a remove server.
+  </div>
+  <div style="margin-bottom: 20px; max-width: 800px">
+    Be aware of false positives (it's possible but unlikely that a class file
+    contains the same bytecode sequence as this tool is looking for) and false
+    negatives (it's possible that a class file is infected but doesn't contain
+    the same bytecode sequence as this tool is looking for).
     <br />
     More information on
     <a href="https://github.com/douira/fractureiser-web-detector">
